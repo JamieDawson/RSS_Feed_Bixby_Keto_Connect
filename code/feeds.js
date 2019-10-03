37ff911785a59063e9c055aca1f0d3d71f9478d5
Jamie Dawson <jadawson@e1z3r4p19.42.us.org>
2019-10-03 15:07:39 -0700

    added image

--- code/feeds.js
+++ code/feeds.js
@@ -4,12 +4,6 @@
 // Tags: What the user can say to automatically call certain feeds
 module.exports = module.exports = [
   {
-    url: 'https://thefantasyfootballers.libsyn.com/fantasyfootball',
-    urlText: 'Click here to see the full story on thefantasyfootballers.libsyn.com',
-    text: 'Example Podcast',
-    tags: ['the fantasy footballers']
-  },
-  {
     url: 'https://www.ketoconnect.net/feed/',
     urlText: 'Click here to see the full story on espn.com',
     text: 'Example Text',
